2101b3b65c60aed66386eeb837b9d027204edfab
davidalvaz <david.alvarezz@gmail.com>
2023-06-16 22:20:38 -0400

    feat: Goroutines and Channels

--- 15-control-channels/main.go
+++ 15-control-channels/main.go
@@ -27,31 +27,47 @@ func (s *Server) sendMessage(msg string) {
 }
 
 func (s *Server) loop() {
+mainloop:
 	for {
 		select {
 		case <-s.quitch:
 			// do some stuff when we need to quit
+			fmt.Println("quitting server")
+			break mainloop
 		case msg := <-s.msgch:
 			// do some stuff when we have a message
 			s.handleMessage(msg)
 		default:
 		}
 	}
+	fmt.Println("server is shutting down")
 }
 
 func (s *Server) handleMessage(msg string) {
 	fmt.Println("we received a message:", msg)
 }
 
+func (s *Server) quit() {
+	s.quitch <- struct{}{} // empty struct
+}
+
 func main() {
 	server := newServer()
-	go server.start()
+
+	go func() {
+		time.Sleep(time.Second * 5)
+		server.quit()
+	}()
+
+	server.start()
+
+	// go server.start()
 
 	// server.sendMessage("hey! do this ...")
-	for i := 0; i < 100; i++ {
-		server.sendMessage(fmt.Sprintf("handle this number %d", i))
-	}
+	// for i := 0; i < 100; i++ {
+	// 	server.sendMessage(fmt.Sprintf("handle this number %d", i))
+	// }
 
-	time.Sleep(time.Second * 5)
+	// time.Sleep(time.Second * 5)
 
 }
