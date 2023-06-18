23cce7d00279118cf4d6dc804a60d67d628527bc
davidalvaz <david.alvarezz@gmail.com>
2023-06-18 18:46:10 -0400

    feat: Mutexes and Atomic Values

--- 16-mutexes-and-atomic-values/main.go
+++ 16-mutexes-and-atomic-values/main.go
@@ -1,17 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	// "sync/atomic"
+)
 
 type State struct {
+	mu    sync.Mutex
 	count int
+	// count int32
+}
+
+func (s *State) setState(i int) {
+	// atomic.AddInt32(&s.count, int32(i))
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.count = i
 }
 
 func main() {
-	state := State{}
+	state := State{} // Init field with default value
 
-	for i := 0; i < 10; i++ {
-		state.count = i
-	}
+	// for i := 0; i < 10; i++ {
+	// 	state.count = i
+	// }
 
 	fmt.Println(state)
 
--- 16-mutexes-and-atomic-values/main_test.go
+++ 16-mutexes-and-atomic-values/main_test.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
 func TestState(t *testing.T) {
-	state := State{}
+	state := &State{}
+	wg := sync.WaitGroup{}
 
 	for i := 0; i < 10; i++ {
-		state.count = i
+		wg.Add(1)
+		// doing some work
+		go func(i int) {
+			// state.count = i + 1
+			state.setState(i + 1)
+			wg.Done() // done working
+		}(i)
 	}
 
+	wg.Wait()
 	fmt.Printf("%+v\n", state)
 
 }
