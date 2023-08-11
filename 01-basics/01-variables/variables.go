e5eb986961031604166b7c9739f9c9251575c5d7
David Alvarez <david.alvarezz@gmail.com>
2023-08-10 23:55:02 -0400

    feat: Variables section

--- 01-basics/01-variables/variables.go
+++ 01-basics/01-variables/variables.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	// Declare variables that are set to their zero value.
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Println("Variables set to their Zero Value")
+	fmt.Printf("var a int \t %T [%v]\n", a, a)
+	fmt.Printf("var b string \t %T [%v]\n", b, b)
+	fmt.Printf("var c float64 \t %T [%v]\n", c, c)
+	fmt.Printf("var d bool \t %T [%v]\n", d, d)
 
 }
